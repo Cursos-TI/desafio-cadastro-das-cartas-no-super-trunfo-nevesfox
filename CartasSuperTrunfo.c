aa556b10bf2a3f88790035eba0dfab873c6dbe37
neves <neves.fox@gmail.com>
2025-08-17 01:03:17 +0000

    SALVAR1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,121 @@
 #include <stdio.h>
+#include <string.h> // Para usar strcspn
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+int main()
+{
+    // VARIAVEIS CARTA 1
+    char nome1[50];
+    char codigocarta1[4];
+    char nomedacidade1[50];
+    float numeropopulacao1;
+    float area1;
+    float pib1;
+    int pontosturisticos1;
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    // VARIAVEIS CARTA 2
+    char nome2[50];
+    char codigocarta2[4];
+    char nomedacidade2[50];
+    float numeropopulacao2;
+    float area2;
+    float pib2;
+    int pontosturisticos2;
+
+    // CARTA 1
+    printf("Digite o Estado 1 \n");
+    fgets(nome1, 50, stdin);
+    nome1[strcspn(nome1, "\n")] = '\0'; // Remove a quebra de linha
+
+    printf("Digite o código da carta 1 \n");
+    fgets(codigocarta1, 4, stdin);
+    codigocarta1[strcspn(codigocarta1, "\n")] = '\0';
+    // Limpa o buffer do teclado caso tenha digitado mais que 3 caracteres
+    if (strlen(codigocarta1) == 3 && codigocarta1[2] != '\n')
+    {
+        while (getchar() != '\n')
+            ; // Limpa o buffer
+    }
+
+    printf("Digite nome da Cidade 1 \n");
+    fgets(nomedacidade1, 50, stdin);
+    nomedacidade1[strcspn(nomedacidade1, "\n")] = '\0';
+
+    printf("Digite o número da população 1 \n");
+    scanf("%f", &numeropopulacao1);
+    while (getchar() != '\n')
+        ; // Limpa o buffer
+
+    printf("Digite o Tamanho da Área 1 \n");
+    scanf("%f", &area1);
+    while (getchar() != '\n')
+        ;
+
+    printf("Digite o PIB 1 \n");
+    scanf("%f", &pib1);
+    while (getchar() != '\n')
+        ;
+
+    printf("Digite a quantidade de Pontos Turisticos 1 \n");
+    scanf("%d", &pontosturisticos1);
+    while (getchar() != '\n')
+        ;
+
+    // CARTA 2
+    printf("Digite o Estado 2 \n");
+    fgets(nome2, 50, stdin);
+    nome2[strcspn(nome2, "\n")] = '\0'; // Remove a quebra de linha
+
+    printf("Digite o código da carta 2 \n");
+    fgets(codigocarta2, 4, stdin);
+    codigocarta2[strcspn(codigocarta2, "\n")] = '\0';
+    // Limpa o buffer do teclado caso tenha digitado mais que 3 caracteres
+    if (strlen(codigocarta2) == 3 && codigocarta2[2] != '\n')
+    {
+        while (getchar() != '\n')
+            ; // Limpa o buffer
+    }
+
+    printf("Digite nome da Cidade 2 \n");
+    fgets(nomedacidade2, 50, stdin);
+    nomedacidade2[strcspn(nomedacidade2, "\n")] = '\0';
+
+    printf("Digite o número da população 2 \n");
+    scanf("%f", &numeropopulacao2);
+    while (getchar() != '\n')
+        ;
+
+    printf("Digite o Tamanho da Área 2 \n");
+    scanf("%f", &area2);
+    while (getchar() != '\n')
+        ;
+
+    printf("Digite o PIB 2 \n");
+    scanf("%f", &pib2);
+    while (getchar() != '\n')
+        ;
+
+    printf("Digite a quantidade de Pontos Turisticos 2 \n");
+    scanf("%d", &pontosturisticos2);
+    while (getchar() != '\n')
+        ;
+
+    // EXIBIÇÃO DOS DADOS DA CARTA 1
+    printf("\nCarta 1 - Estado: %s\n", nome1);
+    printf("Carta 1 - Código: %s\n", codigocarta1);
+    printf("Carta 1 - Nome da Cidade: %s\n", nomedacidade1);
+    printf("Carta 1 - Número da População: %.3f\n", numeropopulacao1);
+    printf("Carta 1 - Tamanho do território: %.3f\n", area1);
+    printf("Carta 1 - PIB: %.3f\n", pib1);
+    printf("Carta 1 - Pontos Turisticos: %d\n", pontosturisticos1);
+
+    // EXIBIÇÃO DOS DADOS DA CARTA 2
+    printf("\nCarta 2 - Estado: %s\n", nome2);
+    printf("Carta 2 - Código: %s\n", codigocarta2);
+    printf("Carta 2 - Nome da Cidade: %s\n", nomedacidade2);
+    printf("Carta 2 - Número da População: %.3f\n", numeropopulacao2);
+    printf("Carta 2 - Tamanho do território: %.3f\n", area2);
+    printf("Carta 2 - PIB: %.3f\n", pib2);
+    printf("Carta 2 - Pontos Turisticos: %d\n", pontosturisticos2);
 
     return 0;
 }
